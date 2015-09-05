f41a143e1e09992a0fc0dfc79a871ee4ff5937bc
Robert Oostenveld <r.oostenveld@gmail.com>
2015-09-05 13:12:58 +0200

    completed first working implementation

--- eegsynt_cvgate/eegsynt_cvgate.ino
+++ eegsynt_cvgate/eegsynt_cvgate.ino
@@ -1,20 +1,35 @@
-const int cv1 = 2;      // the pin controlling the voltage output
-const int cv2 = 3;      // the pin controlling the voltage output
-const int cv3 = 4;      // the pin controlling the voltage output
-const int cv4 = 5;      // the pin controlling the voltage output
+#include <Wire.h>//Include the Wire library to talk I2C
 
-const int gate1 = A0;      // the pin controlling the digital gate
-const int gate2 = A1;      // the pin controlling the digital gate
-const int gate3 = A2;      // the pin controlling the digital gate
-const int gate4 = A3;      // the pin controlling the digital gate
+#define cv1    2      // the pin controlling the voltage output
+#define cv2    3      // the pin controlling the voltage output
+#define cv3    4      // the pin controlling the voltage output
+#define cv4    5      // the pin controlling the voltage output
+#define gate1 A0      // the pin controlling the digital gate
+#define gate2 A1      // the pin controlling the digital gate
+#define gate3 A2      // the pin controlling the digital gate
+#define gate4 A3      // the pin controlling the digital gate
 
 #define NONE    0
 #define VOLTAGE 1
 #define GATE    2
 
+//This is the I2C Address of the MCP4725, by default (A0 pulled to GND).
+//Please note that this breakout is for the MCP4725A0.
+//For devices with A0 pulled HIGH, use 0x61
+#define MCP4725_ADDR 0x60
+
 void setup() {
   // initialize the serial communication:
-  Serial.begin(115200);
+  Serial.begin(57600);
+  Serial.print("\n[eegsynt_cvgate / ");
+  Serial.print(__DATE__);
+  Serial.print(" / ");
+  Serial.print(__TIME__);
+  Serial.println("]");
+  Serial.setTimeout(1000);
+
+  Wire.begin();
+
   // initialize the gates as an output:
   pinMode(gate1, OUTPUT);
   pinMode(gate2, OUTPUT);
@@ -23,34 +38,35 @@ void setup() {
 }
 
 void loop() {
-  byte b, command = NONE;
+  byte b, channel = 0, command = NONE;
+  int value = 0;
 
   if (Serial.available()) {
 
-    // possible sequences of characters are 
+    // possible sequences of characters are
     // *c1v1024#  control 1 voltage 5*1024/4096 = 1.25 V
     // *g1v1#     gate 1 value ON
 
-    b = Serial.read(); 
-    if (b=='*') {
-      b = Serial.read();
-      if (b=='c') {
+    b = Serial.read();
+    if (b == '*') {
+      Serial.readBytes(&b, 1);
+      if (b == 'c') {
         command = VOLTAGE;
-        b = Serial.read(); channel = b-48; // character '1' is ascii value 49 
-        b = Serial.read(); // 'v'
-        b = Serial.read(); value  = (b-48)*1000;
-        b = Serial.read(); value += (b-48)*100;
-        b = Serial.read(); value += (b-48)*10;
-        b = Serial.read(); value += (b-48)*1;
-        b = Serial.read(); value += (b-48)*1;
-        b = Serial.read(); command = (b=='#' ? command : NONE);
+        value = 0;
+        Serial.readBytes(&b, 1); channel = b - 48; // character '1' is ascii value 49
+        Serial.readBytes(&b, 1); // 'v'
+        Serial.readBytes(&b, 1); value  = (b - 48) * 1000;
+        Serial.readBytes(&b, 1); value += (b - 48) * 100;
+        Serial.readBytes(&b, 1); value += (b - 48) * 10;
+        Serial.readBytes(&b, 1); value += (b - 48) * 1;
+        Serial.readBytes(&b, 1); command = (b == '#' ? command : NONE);
       }
-      else if (b=='g') {
+      else if (b == 'g') {
         command = GATE;
-        b = Serial.read(); channel = b-48; // character '1' is ascii value 49 
-        b = Serial.read(); // 'v'
-        b = Serial.read(); value  = (b=='1');
-        b = Serial.read(); command = (b=='#' ? command : NONE);
+        Serial.readBytes(&b, 1); channel = b - 48; // character '1' is ascii value 49
+        Serial.readBytes(&b, 1); // 'v'
+        Serial.readBytes(&b, 1); value  = (b == '1');
+        Serial.readBytes(&b, 1); command = (b == '#' ? command : NONE);
       }
       else {
         command = NONE;
@@ -59,11 +75,44 @@ void loop() {
     else {
       command = NONE;
     }
-  }
 
-  if (command==VOLTAGE) {
-  }
-  else if (command==GATE) {
+    if (command == VOLTAGE) {
+      Serial.print("setting control voltage ");
+      Serial.print(channel);
+      Serial.print(" to ");
+      Serial.println(value);
+      Wire.beginTransmission(MCP4725_ADDR);
+      Wire.write(64);                      // cmd to update the DAC
+      Wire.write((value     ) >> 4);       // the 8 most significant bits...
+      Wire.write((value & 15) << 4);       // the 4 least significant bits...
+      Wire.endTransmission();
+
+
+    }
+    else if (command == GATE) {
+      Serial.print("setting gate ");
+      Serial.print(channel);
+      Serial.print(" to ");
+      Serial.println(value != 0);
+      switch (channel) {
+        case 1:
+          digitalWrite(gate1, value != 0);
+          break;
+        case 2:
+          digitalWrite(gate2, value != 0);
+          break;
+        case 3:
+          digitalWrite(gate3, value != 0);
+          break;
+        case 4:
+          digitalWrite(gate4, value != 0);
+          break;
+      }
+    }
+    else {
+      Serial.println("invalid command");
+    }
   }
 }
+
 
