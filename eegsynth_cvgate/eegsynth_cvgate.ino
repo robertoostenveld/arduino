2187d75bfef7fe815e312580d91b4e1c0f1fbe85
Robert Oostenveld <r.oostenveld@gmail.com>
2015-09-20 13:54:08 +0200

    extended with LF398N sample and hold ICs to allow for 4 output control voltages. Less verbose output, this speeds up the max refresh from ~100 to 250 Hz.

--- eegsynth_cvgate/eegsynth_cvgate.ino
+++ eegsynth_cvgate/eegsynth_cvgate.ino
@@ -1,22 +1,47 @@
 #include <Wire.h>//Include the Wire library to talk I2C
 
-#define cv1    2      // the pin controlling the voltage output
-#define cv2    3      // the pin controlling the voltage output
-#define cv3    4      // the pin controlling the voltage output
-#define cv4    5      // the pin controlling the voltage output
-#define gate1 A0      // the pin controlling the digital gate
-#define gate2 A1      // the pin controlling the digital gate
-#define gate3 A2      // the pin controlling the digital gate
-#define gate4 A3      // the pin controlling the digital gate
+#define voltage1pin    2      // the pin controlling the voltage output
+#define voltage2pin    3      // the pin controlling the voltage output
+#define voltage3pin    4      // the pin controlling the voltage output
+#define voltage4pin    5      // the pin controlling the voltage output
+#define gate1pin A0      // the pin controlling the digital gate
+#define gate2pin A1      // the pin controlling the digital gate
+#define gate3pin A2      // the pin controlling the digital gate
+#define gate4pin A3      // the pin controlling the digital gate
+
+#define MCP4726_CMD_WRITEDAC            (0x40)  // Writes data to the DAC
+#define MCP4726_CMD_WRITEDACEEPROM      (0x60)  // Writes data to the DAC and the EEPROM (persisting the assigned value after reset)
 
 #define NONE    0
 #define VOLTAGE 1
 #define GATE    2
 
+#define OK    1
+#define ERROR 2
+
 //This is the I2C Address of the MCP4725, by default (A0 pulled to GND).
 //Please note that this breakout is for the MCP4725A0.
 //For devices with A0 pulled HIGH, use 0x61
 #define MCP4725_ADDR 0x60
+
+// these remember the state of all CV and gate outputs
+int voltage1 = 0, voltage2 = 0, voltage3 = 0, voltage4 = 0;
+int gate1 = 0, gate2 = 0, gate3 = 0, gate4 = 0;
+
+void sampleAndHold(int pin, uint16_t value) {
+  uint8_t msb, lsb;
+  msb = (value / 16);
+  lsb = (value % 16) << 4;
+  Wire.beginTransmission(MCP4725_ADDR);
+  Wire.write(MCP4726_CMD_WRITEDAC);   // cmd to update the DAC
+  Wire.write(msb);                    // the 8 most significant bits...
+  Wire.write(lsb);                    // the 4 least significant bits...
+  Wire.endTransmission();
+  digitalWrite(pin, HIGH);
+  delay(1);                           // give it some time to Sample and Hold
+  digitalWrite(pin, LOW);
+  return;
+}
 
 void setup() {
   // initialize the serial communication:
@@ -30,21 +55,27 @@ void setup() {
 
   Wire.begin();
 
-  // initialize the gates as an output:
-  pinMode(gate1, OUTPUT);
-  pinMode(gate2, OUTPUT);
-  pinMode(gate3, OUTPUT);
-  pinMode(gate4, OUTPUT);
+  // initialize the gate pins as output:
+  pinMode(gate1pin, OUTPUT);
+  pinMode(gate2pin, OUTPUT);
+  pinMode(gate3pin, OUTPUT);
+  pinMode(gate4pin, OUTPUT);
+
+  // initialize the control voltage pins as output:
+  pinMode(voltage1pin, OUTPUT);
+  pinMode(voltage2pin, OUTPUT);
+  pinMode(voltage3pin, OUTPUT);
+  pinMode(voltage4pin, OUTPUT);
 }
 
 void loop() {
-  byte b, channel = 0, command = NONE;
+  byte b, channel = 0, command = NONE, status = OK;
   int value = 0;
 
   if (Serial.available()) {
 
     // possible sequences of characters are
-    // *c1v1024#  control 1 voltage 5*1024/4096 = 1.25 V
+    // *c1v1024#  control 1 voltage 5*1024/4095 = 1.25 V
     // *g1v1#     gate 1 value ON
 
     b = Serial.read();
@@ -76,43 +107,71 @@ void loop() {
       command = NONE;
     }
 
+    // update the internal state of all output channels
     if (command == VOLTAGE) {
-      Serial.print("setting control voltage ");
-      Serial.print(channel);
-      Serial.print(" to ");
-      Serial.println(value);
-      Wire.beginTransmission(MCP4725_ADDR);
-      Wire.write(64);                      // cmd to update the DAC
-      Wire.write((value     ) >> 4);       // the 8 most significant bits...
-      Wire.write((value & 15) << 4);       // the 4 least significant bits...
-      Wire.endTransmission();
-
+      switch (channel) {
+        case 1:
+          voltage1 = value;
+          status = OK;
+          break;
+        case 2:
+          voltage2 = value;
+          status = OK;
+          break;
+        case 3:
+          voltage3 = value;
+          status = OK;
+          break;
+        case 4:
+          voltage4 = value;
+          status = OK;
+          break;
+        default:
+          status = ERROR;
+      }
 
     }
     else if (command == GATE) {
-      Serial.print("setting gate ");
-      Serial.print(channel);
-      Serial.print(" to ");
-      Serial.println(value != 0);
       switch (channel) {
         case 1:
-          digitalWrite(gate1, value != 0);
+          gate1 = (value != 0);
+          status = OK;
           break;
         case 2:
-          digitalWrite(gate2, value != 0);
+          gate2 = (value != 0);
+          status = OK;
           break;
         case 3:
-          digitalWrite(gate3, value != 0);
+          gate3 = (value != 0);
+          status = OK;
           break;
         case 4:
-          digitalWrite(gate4, value != 0);
+          gate4 = (value != 0);
+          status = OK;
           break;
+        default:
+          status = ERROR;
       }
     }
     else {
-      Serial.println("invalid command");
+      status = ERROR;
     }
+    if (status == OK)
+      Serial.println("ok");
+    else if (status == ERROR)
+      Serial.println("error");
   }
-}
+  else {
+    // refresh all output channels
+    sampleAndHold(voltage1pin, voltage1);
+    sampleAndHold(voltage2pin, voltage2);
+    sampleAndHold(voltage3pin, voltage3);
+    sampleAndHold(voltage4pin, voltage4);
+    digitalWrite(gate1pin, gate1);
+    digitalWrite(gate2pin, gate2);
+    digitalWrite(gate3pin, gate3);
+    digitalWrite(gate4pin, gate4);
+  }
+} //main
 
 
