684782fe06ddc845a25bd72d38c08839dd79b5f5
Robert Oostenveld <r.oostenveld@gmail.com>
2017-04-17 09:38:38 +0200

    first working version of Artnet-DMX512 relay, mpstly based on esp8266_artnet_neopixel

---
+++ esp8266_artnet_dmx512/esp8266_artnet_dmx512.ino
@@ -0,0 +1,291 @@
+/*
+  This sketch receives the data of one DMX universes via Artnet and
+  sends it over the serial interface to a MAX485 module
+
+  https://github.com/rstephan/ArtnetWifi
+*/
+
+#include <ESP8266WiFi.h>         // https://github.com/esp8266/Arduino
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <WiFiManager.h>         // https://github.com/tzapu/WiFiManager
+#include <WiFiClient.h>
+#include <ArtnetWifi.h>          // https://github.com/rstephan/ArtnetWifi 
+#include <FS.h>
+
+#include "setup_ota.h"
+
+Config config;
+ESP8266WebServer server(80);
+const char* host = "ARTNET";
+const char* version = __DATE__ " / " __TIME__;
+
+#define LED_R 16  // D0
+#define LED_G 5   // D1
+#define LED_B 4   // D2
+
+// Artnet settings
+ArtnetWifi artnet;
+unsigned long packetCounter = 0, frameCounter = 0;
+float fps = 0;
+
+// Global universe buffer
+struct {
+  uint16_t universe;
+  uint16_t length;
+  uint8_t sequence;
+  uint8_t *data;
+} global;
+
+// keep reack of the timing of the function calls
+long tic_loop = 0, tic_fps = 0, tic_packet = 0, tic_web = 0;
+
+//this will be called for each UDP packet received
+void onDmxPacket(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t * data) {
+  // print some feedback
+  Serial.print("packetCounter = ");
+  Serial.print(packetCounter++);
+  if ((millis() - tic_fps) > 1000 && frameCounter > 100) {
+    // don't estimate the FPS too frequently
+    fps = 1000 * frameCounter / (millis() - tic_fps);
+    tic_fps = millis();
+    frameCounter = 0;
+    Serial.print(",  FPS = ");
+    Serial.print(fps);
+  }
+  Serial.println();
+
+  if (universe == config.universe) {
+    // copy the data from the UDP packet over to the global universe buffer
+    global.universe = universe;
+    global.sequence = sequence;
+    if (length < 512)
+      global.length = length;
+    for (int i = 0; i < global.length; i++)
+      global.data[i] = data[i];
+  }
+} // onDmxpacket
+
+
+void setup() {
+  Serial1.begin(250000); // this connects over D4/TXD1/GPIO02 to the MAX485 module
+  Serial.begin(115200);
+  while (!Serial) {
+    ;
+  }
+  Serial.println("setup starting");
+
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
+
+  global.universe = 0;
+  global.sequence = 0;
+  global.length = 512;
+  global.data = (uint8_t *)malloc(512);
+  for (int i=0; i<512; i++)
+    global.data[i] = 0;
+
+  SPIFFS.begin();
+
+  initialConfig();
+
+  if (loadConfig()) {
+    singleYellow();
+    delay(1000);
+  }
+  else {
+    singleRed();
+    delay(1000);
+  }
+
+  WiFiManager wifiManager;
+  // wifiManager.resetSettings();
+  wifiManager.setAPStaticIPConfig(IPAddress(192, 168, 1, 1), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
+  wifiManager.autoConnect(host);
+  Serial.println("connected");
+
+  if (WiFi.status() == WL_CONNECTED)
+    singleGreen();
+
+  // this serves all URIs that can be resolved to a file on the SPIFFS filesystem
+  server.onNotFound(handleNotFound);
+
+  server.on("/", HTTP_GET, []() {
+    tic_web = millis();
+    handleRedirect("/index");
+  });
+
+  server.on("/index", HTTP_GET, []() {
+    tic_web = millis();
+    handleStaticFile("/index.html");
+  });
+
+  server.on("/defaults", HTTP_GET, []() {
+    tic_web = millis();
+    Serial.println("handleDefaults");
+    handleStaticFile("/reload_success.html");
+    delay(2000);
+    singleRed();
+    initialConfig();
+    saveConfig();
+    WiFiManager wifiManager;
+    wifiManager.resetSettings();
+    ESP.restart();
+  });
+
+  server.on("/reconnect", HTTP_GET, []() {
+    tic_web = millis();
+    Serial.println("handleReconnect");
+    handleStaticFile("/reload_success.html");
+    delay(2000);
+    singleYellow();
+    WiFiManager wifiManager;
+    wifiManager.setAPStaticIPConfig(IPAddress(192, 168, 1, 1), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
+    wifiManager.startConfigPortal(host);
+    Serial.println("connected");
+    if (WiFi.status() == WL_CONNECTED)
+      singleGreen();
+  });
+
+  server.on("/reset", HTTP_GET, []() {
+    tic_web = millis();
+    Serial.println("handleReset");
+    handleStaticFile("/reload_success.html");
+    delay(2000);
+    singleRed();
+    ESP.restart();
+  });
+
+  server.on("/monitor", HTTP_GET, [] {
+    tic_web = millis();
+    handleStaticFile("/monitor.html");
+  });
+
+  server.on("/hello", HTTP_GET, [] {
+    tic_web = millis();
+    handleStaticFile("/hello.html");
+  });
+
+  server.on("/settings", HTTP_GET, [] {
+    tic_web = millis();
+    handleStaticFile("/settings.html");
+  });
+
+  server.on("/dir", HTTP_GET, [] {
+    tic_web = millis();
+    handleDirList();
+  });
+
+  server.on("/json", HTTP_PUT, [] {
+    tic_web = millis();
+    handleJSON();
+  });
+
+  server.on("/json", HTTP_POST, [] {
+    tic_web = millis();
+    handleJSON();
+  });
+
+  server.on("/json", HTTP_GET, [] {
+    tic_web = millis();
+    StaticJsonBuffer<300> jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+    CONFIG_TO_JSON(universe, "universe");
+    root["uptime"]  = long(millis() / 1000);
+    root["packets"] = packetCounter;
+    root["fps"]     = fps;
+    String str;
+    root.printTo(str);
+    server.send(200, "application/json", str);
+  });
+
+  server.on("/update", HTTP_GET, [] {
+    tic_web = millis();
+    handleStaticFile("/update.html");
+  });
+
+  server.on("/update", HTTP_POST, handleUpdate1, handleUpdate2);
+
+  // start the web server
+  server.begin();
+
+  // announce the hostname and web server through zeroconf
+  MDNS.begin(host);
+  MDNS.addService("http", "tcp", 80);
+
+  artnet.begin();
+  artnet.setArtDmxCallback(onDmxPacket);
+
+  // initialize all timers
+  tic_loop   = millis();
+  tic_packet = millis();
+  tic_fps    = millis();
+  tic_web    = 0;
+
+  Serial.println("setup done");
+} // setup
+
+void loop() {
+  server.handleClient();
+
+  if (WiFi.status() != WL_CONNECTED) {
+    singleRed();
+  }
+  else if ((millis() - tic_web) < 5000) {
+    singleBlue();
+  }
+  else  {
+    artnet.read();
+
+    // this section gets executed at a maximum rate of around 40Hz
+    if ((millis() - tic_loop) > 25) {
+      tic_loop = millis();
+      frameCounter++;
+
+      // Send "break" as a "slow" zero.
+      Serial1.begin(56700);
+      Serial1.write(0);
+      Serial1.begin(250000);
+
+      Serial1.write(0); // Start-Byte
+      // send out the buffer
+      for (int i = 0; i < global.length; i++)
+        Serial1.write(global.data[i]);
+    }
+  }
+
+  delay(1);
+} // loop
+
+
+void singleRed() {
+  digitalWrite(LED_R, HIGH);
+  digitalWrite(LED_G, LOW);
+  digitalWrite(LED_B, LOW);
+}
+
+void singleGreen() {
+  digitalWrite(LED_R, LOW);
+  digitalWrite(LED_G, HIGH);
+  digitalWrite(LED_B, LOW);
+}
+
+void singleBlue() {
+  digitalWrite(LED_R, LOW);
+  digitalWrite(LED_G, LOW);
+  digitalWrite(LED_B, HIGH);
+}
+
+void singleYellow() {
+  digitalWrite(LED_R, LOW);
+  digitalWrite(LED_G, HIGH);
+  digitalWrite(LED_B, HIGH);
+}
+
+void allBlack() {
+  digitalWrite(LED_R, LOW);
+  digitalWrite(LED_G, LOW);
+  digitalWrite(LED_B, LOW);
+}
+
---
+++ esp8266_artnet_dmx512/setup_ota.h
@@ -0,0 +1,35 @@
+#ifndef _SETUP_OTA_H_
+#define _SETUP_OTA_H_
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <String.h>
+#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+#include <FS.h>
+
+#define JSON_TO_CONFIG(x, y)   { if (root.containsKey(y)) { config.x = root[y]; } }
+#define CONFIG_TO_JSON(x, y)   { root[y] = config.x; }
+#define KEYVAL_TO_CONFIG(x, y) { if (server.hasArg(y))    { String str = server.arg(y); config.x = str.toInt(); } }
+
+
+struct Config {
+  int universe;
+};
+
+bool initialConfig(void);
+bool loadConfig(void);
+bool saveConfig(void);
+
+void handleUpdate1(void);
+void handleUpdate2(void);
+void handleDirList(void);
+void handleNotFound(void);
+void handleRedirect(String);
+void handleRedirect(const char *);
+void handleStaticFile(String);
+void handleStaticFile(const char *);
+void handleJSON();
+
+#endif // _SETUP_OTA_H_
+
