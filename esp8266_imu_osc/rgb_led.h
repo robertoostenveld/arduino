af8db9533665d6704fed52f8530da2c35dd18052
Robert Oostenveld <r.oostenveld@gmail.com>
2018-04-22 11:40:11 +0200

    updated LED pins

--- esp8266_imu_osc/rgb_led.h
+++ esp8266_imu_osc/rgb_led.h
@@ -1,9 +1,9 @@
 #ifndef _RGB_LED_H_
 #define _RGB_LED_H_
 
-#define LED_R D6
-#define LED_G LED_BUILTIN
-#define LED_B D8
+#define LED_R D5
+#define LED_G D6
+#define LED_B D7
 
 // #define COMMON_ANODE
 
