85f67066074f5247696cccf2d7b0da3137a500c6
Robert Oostenveld <r.oostenveld@gmail.com>
2019-10-27 14:40:45 +0100

    use WiFiManager for configuration of the wifi network, use D6 instead of D5

--- esp8266_12v_trigger/esp8266_12v_trigger.ino
+++ esp8266_12v_trigger/esp8266_12v_trigger.ino
@@ -2,9 +2,8 @@
  *  This sketch serves as a 12 volt trigger to switch a NAD-D3020 audio amplifier on and off.
  *  The on and off state are controlled over a http call. The current status can also be probed.
  *
- *  The sketch is designed to run on a Wemos D1 mini that has pin 15 connected to a NPN transistor.
- *  Although it is designed for the 12V trigger input, the output voltage can be toggled
- *  between 0 and 5 volt. This is enough to trigget the amplifier.
+ *  The GPOI output of pin D5 is used to switch a PC900v optocoupler. Its output voltage can be 
+ *  toggled between 0 and 5 volt, which is is enough to trigger the amplifier.
  *
  *  Valid HTTP calls are
  *    http://nad-d3020.local/on
@@ -14,7 +13,7 @@
  *    http://nad-d3020.local/reset
  *    http://nad-d3020.local/         this returns an identifier
  *
- *  (C) 2017 Robert Oostenveld
+ *  (C) 2017-2019 Robert Oostenveld
  *
  */
 
@@ -22,21 +21,19 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include "secret.h"
+#include <DNSServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
-#define PINOUT 14 // this corresponds to D5 on the Wemos D1 mini
-
-// These are defined in secret.h
-// const char* ssid     = "xxxx";
-// const char* password = "xxxx";
+#define PINOUT D6
 
 const char* version = __DATE__ " / " __TIME__;
+const char* name = "NAD-D3020";
 
 ESP8266WebServer server(80);
 
 void handleRoot() {
   Serial.println("/");
-  server.send(200, "text/plain", "NAD-D3020");
+  server.send(200, "text/plain", name);
 }
 
 void handleNotFound() {
@@ -58,40 +55,27 @@ long checkpoint = millis();
 
 void setup(void) {
   pinMode(PINOUT, OUTPUT);
-  digitalWrite(PINOUT, LOW);
+  digitalWrite(PINOUT, HIGH); // note that the logic is inverted
 
   Serial.begin(115200);
   while (!Serial) {
     ;
   }
-
+ 
   Serial.print("\n[esp8266_12v_trigger / ");
   Serial.print(version);
   Serial.println("]");
 
-  WiFi.mode(WIFI_STA);
+  WiFiManager wifiManager;
+  //wifiManager.resetSettings();
+  wifiManager.autoConnect(name);
 
-  WiFi.begin(ssid, password);
   Serial.println("");
-
-  // Wait for connection
-  int count = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    if (count++>10) {
-      Serial.println("reset");
-      ESP.reset();
-    }
-    else
-      Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
+  Serial.println("Connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("NAD-D3020")) {
+  if (MDNS.begin(name)) {
     Serial.println("MDNS responder started");
   }
 
@@ -99,13 +83,13 @@ void setup(void) {
 
   server.on("/on", []() {
     Serial.println("/on");
-    digitalWrite(PINOUT, HIGH);
+    digitalWrite(PINOUT, LOW); // note that the logic is inverted
     server.send(200, "text/plain", "on");
   });
 
   server.on("/off", []() {
     Serial.println("/off");
-    digitalWrite(PINOUT, LOW);
+    digitalWrite(PINOUT, HIGH); // note that the logic is inverted
     server.send(200, "text/plain", "off");
   });
 
