b490b6663b4f52c584cf8c270831d7eecb2408b5
Robert Oostenveld <r.oostenveld@gmail.com>
2024-11-17 22:07:43 +0100

    made an example that is compatible with ArduinoJson version 6

---
+++ esp32_config_webinterface_v6/README.md
@@ -0,0 +1,25 @@
+# Overview
+
+This is a demonstration and test sketch for configuring persistent options via the web interface. This strategy is used in a number of my functional ESP32 and ESP8266 sketches. It consists of a `settings.json` file in the SPIFFS filesystem and a `settings.html` file with some javascript.
+
+The settings can be changed in the webbrowser at http://192.168.1.xxx/settings and clicking "Send". This results in `Webserver` parsing the arguments and placing the variables in the header, but also places the query string in the body.
+
+The following also results in `Webserver` parsing the arguments and placing the variables in the header.
+
+    curl -X POST 'http://192.168.1.xxx/json?var1=11&var2=22&var3=33'
+    curl -X PUT  'http://192.168.1.xxx/json?var1=11&var2=22&var3=33'
+
+The following is not parsed, but only places the JSON data in the body.
+
+    curl -X POST http://192.168.1.xxx/json -d '{"var1":11,"var2":22,"var3":33}'
+    curl -X PUT  http://192.168.1.xxx/json -d '{"var1":11,"var2":22,"var3":33}'
+
+## SPIFFS for static files
+
+You should not only write the firmware to the ESP32 module, but also the static content for the web interface. The html, css and javascript files located in the data directory should be written to the SPIFS filesystem on the ESP32. See https://github.com/me-no-dev/arduino-esp32fs-plugin.
+
+You will get a "file not found" error if the firmware cannot access the data files.
+
+## Arduino ESP32 filesystem uploader
+
+This Arduino sketch includes a `data` directory with a number of files that should be uploaded to the ESP32 using the [SPIFFS filesystem uploader](https://github.com/me-no-dev/arduino-esp32fs-plugin) tool. At the moment (Feb 2024) the Arduino 2.x IDE does *not* support the SPIFFS filesystem uploader plugin. You have to use the Arduino 1.8.x IDE (recommended), or the command line utilities for uploading the data.
---
+++ esp32_config_webinterface_v6/esp32_config_webinterface_v6.ino
@@ -0,0 +1,116 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>  // https://arduinojson.org
+#include <WiFi.h>
+#include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <FS.h>
+#include <SPIFFS.h>
+
+#include "webinterface.h"
+
+/*********************************************************************************/
+
+const char* host = "ESP32";
+const char* version = __DATE__ " / " __TIME__;
+
+Config config;
+WebServer server(80);
+WiFiManager wifiManager;
+
+/*********************************************************************************/
+
+void setup() {
+
+  Serial.begin(115200);
+  while (!Serial) {
+    ;
+  }
+  Serial.println("Setup starting");
+
+  // The SPIFFS file system contains the config.json, and the html and javascript code for the web interface
+  SPIFFS.begin();
+  loadConfig();
+  printConfig();
+
+  WiFiManager wifiManager;
+  wifiManager.setAPStaticIPConfig(IPAddress(192, 168, 1, 1), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
+  wifiManager.autoConnect(host);
+  Serial.println("connected");
+
+  // this serves all URIs that can be resolved to a file on the SPIFFS filesystem
+  server.onNotFound(handleNotFound);
+
+  server.on("/", HTTP_GET, []() {
+    handleRedirect("/index.html");
+  });
+
+  server.on("/defaults", HTTP_GET, []() {
+    Serial.println("handleDefaults");
+    handleStaticFile("/reload_success.html");
+    defaultConfig();
+    printConfig();
+    saveConfig();
+    server.close();
+    server.stop();
+    ESP.restart();
+  });
+
+  server.on("/reconnect", HTTP_GET, []() {
+    Serial.println("handleReconnect");
+    handleStaticFile("/reload_success.html");
+    server.close();
+    server.stop();
+    delay(5000);
+    WiFiManager wifiManager;
+    wifiManager.resetSettings();
+    wifiManager.setAPStaticIPConfig(IPAddress(192, 168, 1, 1), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
+    wifiManager.startConfigPortal(host);
+    server.begin();
+    if (WiFi.status() == WL_CONNECTED)
+      Serial.println("WiFi status ok");
+  });
+
+  server.on("/restart", HTTP_GET, []() {
+    Serial.println("handleRestart");
+    handleStaticFile("/reload_success.html");
+    server.close();
+    server.stop();
+    SPIFFS.end();
+    delay(5000);
+    ESP.restart();
+  });
+
+  server.on("/json", HTTP_PUT, handleJSON);
+
+  server.on("/json", HTTP_POST, handleJSON);
+
+  server.on("/json", HTTP_GET, [] {
+    DynamicJsonDocument root(300);
+    N_CONFIG_TO_JSON(var1, "var1");
+    N_CONFIG_TO_JSON(var2, "var2");
+    N_CONFIG_TO_JSON(var3, "var3");
+    root["version"] = version;
+    root["uptime"] = long(millis() / 1000);
+    String str;
+    serializeJson(root, str);
+    server.send(200, "application/json", str);
+  });
+
+  // start the web server
+  server.begin();
+
+  // announce the hostname and web server through zeroconf
+  MDNS.begin(host);
+  MDNS.addService("http", "tcp", 80);
+
+  Serial.println("Setup done");
+}
+
+/*********************************************************************************/
+
+void loop() {
+  server.handleClient();
+  delay(10);  // in milliseconds
+}
