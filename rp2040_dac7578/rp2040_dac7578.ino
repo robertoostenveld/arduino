da265a627ab98f8c6d49c41d07e8d3a011cae3ea
Robert Oostenveld <r.oostenveld@gmail.com>
2025-12-11 11:55:18 +0100

    don't wait indefinitely for serial interface to start

--- rp2040_dac7578/rp2040_dac7578.ino
+++ rp2040_dac7578/rp2040_dac7578.ino
@@ -19,24 +19,28 @@ Adafruit_DACX578 dac0(12);
 Adafruit_DACX578 dac1(12);
 
 const float frequency[16] = { 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 };  // Hz
-const uint16_t amplitude = 2048;                                                         // Half of full scale for 12-bit dac0 (0 to 4095)
-const uint16_t offset = 2048;                                                            // DC offset to keep sine wave positive
-const uint32_t sampleTime = 1000;                                                        // Approximate time between samples, in microseconds
-const uint32_t feedbackTime = -1;                                                        // Time between serial feedback, in microseconds
+const uint16_t amplitude = 2048;                                                                 // Half of full scale for 12-bit dac0 (0 to 4095)
+const uint16_t offset = 2048;                                                                    // DC offset to keep sine wave positive
+const uint32_t sampleTime = 1000;                                                                // Approximate time between samples, in microseconds
+const uint32_t feedbackTime = -1;                                                                // Time between serial feedback, in microseconds
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial)
-    delay(10);
-
-  Serial.println("------------------------------");
-  Serial.println("16 channel sine wave generator");
-
+  // blink rapidly, this will continue indefinitely when the setup fails
   ledInit();
-
-  // blink quickly for the first second, this will continue indefinitely when the setup fails
   ledFast();
-  delay(1000);
+
+  Serial.begin(115200);
+  while (!Serial && (millis() < 5000)) {
+    // wait for the serial interface to start, but not longer than 5 seconds
+    delay(100);
+  }
+
+  Serial.println("-------------------------------");
+  Serial.println("16 channel sine wave generator");
+  Serial.print("firmware ");
+  Serial.print(__TIME__);
+  Serial.print(" / ");
+  Serial.println(__DATE__);
 
   // initialize the first DACX578
   while (!dac0.begin((uint8_t)0x47, &Wire)) {
@@ -63,7 +67,7 @@ void setup() {
   // set I2C frequency to 3.4 MHz for faster communication
   Wire.setClock(3400000);
 
-  // blink slowly, this will continue for the rest of the time
+  // blink slowly for the rest of the time
   ledSlow();
 }
 
@@ -91,7 +95,7 @@ void loop() {
         dac0.writeAndUpdateChannelValue(channel, (uint16_t)sineValue);
       } else {
         // channels 8-15 correspond to channels 0-7 on the second DAC
-        dac1.writeAndUpdateChannelValue(channel-8, (uint16_t)sineValue);
+        dac1.writeAndUpdateChannelValue(channel - 8, (uint16_t)sineValue);
       }
     }
     lastSample = currentTime;
