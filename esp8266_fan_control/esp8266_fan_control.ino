8300c1faade7552ef5771e91c49f0f336fb1e7c1
Robert Oostenveld <r.oostenveld@gmail.com>
2026-01-04 16:09:21 +0100

    increase pwm frequency

--- esp8266_fan_control/esp8266_fan_control.ino
+++ esp8266_fan_control/esp8266_fan_control.ino
@@ -1,10 +1,11 @@
 /*
   This sketch is for an ESP8266 connected to an ARCTIC BioniX F140
   140 mm diameter fan with PWM control.
+
+  See https://www.arctic.de/media/55/b2/e4/1690270670/Spec_Sheet_BioniX_F140_EN.pdf
   
-  The sketch controls the speed of the fan and uses an
-  interrupt to measure the speed. Output is provided on
-  the serial interface.
+  The sketch controls the speed of the fan and uses an interrupt 
+  to measure the speed. Output is provided on the serial interface.
 
 */
 
@@ -13,7 +14,7 @@
 const int controlPin = D1;
 const int sensorPin = D2;
 
-void ICACHE_RAM_ATTR callback ();
+void ICACHE_RAM_ATTR callback();
 
 const char* version = __DATE__ " / " __TIME__;
 
@@ -28,30 +29,33 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
-  analogWriteFreq(25000);
+  analogWriteFreq(40000);
   pinMode(controlPin, OUTPUT);
-  analogWrite(controlPin, 255);
+  analogWrite(controlPin, 255);  // this is where the speed is controlled
 
   pinMode(sensorPin, INPUT);
   attachInterrupt(digitalPinToInterrupt(sensorPin), callback, RISING);
 }
 
 void loop() {
-  delay(1000);
-  
+  unsigned int rps, rpm;
+
   noInterrupts();
-  unsigned int rpm, counts = counter;
+  rps = counter;
   counter = 0;
   interrupts();
 
-  rpm = (counter / 2) * 60;
+  // convert from counts per second to rotations per minute
+  rpm = rps * 60 / 2;
 
   Serial.print("Counts: ");
-  Serial.print(counts, 1);
+  Serial.print(rps, 1);
   Serial.print(" RPM: ");
   Serial.println(rpm);
-  
+
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
+  delay(1000);  // run once per second
 }
 
 void callback() {
