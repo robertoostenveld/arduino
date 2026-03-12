92ecbf7dd6d5426f8f6fc37dd7b9ff83f4006480
Robert Oostenveld <r.oostenveld@gmail.com>
2026-03-12 20:40:40 +0100

    implement the trigger pulse length, in line with Bitsi v94
    - levelmode commands are recognized as such, but not supported otherwise
    - specify OUTPUT_PINS as a list

--- bitsi/bitsi.ino
+++ bitsi/bitsi.ino
@@ -8,17 +8,29 @@
  * This code originates from https://code.google.com/archive/p/dccn-lab/ where it was released under the GNU GPL v2
  *
  * Copyright (C) 2009-2010 Erik van der Boogert & Bram Daams (for the original implementation)
- * Copyright (C) 2025 Robert Oostenveld (for this cleaned up version)  
+ * Copyright (C) 2025-2026 Robert Oostenveld (for this cleaned up version)  
  *
+ * From version v94, the Bitsi trigger pulse length can be programmed. In theory, the pulse length can be 
+ * between 1 and 255 ms. Therefore we send three codes in sequence to the Bitsi: [0], [1], [pulselength]. 
+ * By sending [0], we put the Bitsi in programming mode. Sending [1] tells the Bitsi we want to change 
+ * the pulse length, and the [pulselength] will be the value we want the trigger pulse to last.
+ * 
+ * Version v94 also added the level mode to the functionality of the Bitsi. This means that when you 
+ * trigger an output, the output will remain 'high', until you send another code or reset the output.
+ * The level mode functionality is NOT SUPPORTED by this specific implementation of the Bitsi firmware.
+ *  
  */
 
 // Constants
-const unsigned long TIMEOUT_PULSE_LENGTH = 15;  // Pulse length in ms
-const unsigned long TIMEOUT_DEBOUNCE = 8;       // Debounce interval in ms
+const unsigned long TIMEOUT_DEBOUNCE = 8; // Debounce interval in ms
 const int PIN_BUILTIN_LED = 13;
 
 // Input pins configuration
 const int INPUT_PINS[8] = {10, 11, 12, 14, 15, 16, 17, 18};
+
+// Output pins configuration
+const int OUTPUT_PINS[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+unsigned long TIMEOUT_PULSE_LENGTH = 15;  // Pulse length in ms
 
 // Variables
 byte serialInChar;                  // To receive desired Parallel_Out value
@@ -34,9 +46,21 @@ bool prevInputState[8] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
 unsigned long debounceStartTime[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 bool isDebouncing[8] = {false, false, false, false, false, false, false, false};
 
+// For initialization of the duration and/or levelmode programming
+typedef enum operatingMode {
+  NORMAL,
+  PROGRAMMING,
+  PULSELENGTH,
+  LEVELMODE
+} operatingMode_t;
+operatingMode_t operatingMode;
+
 void setup() {
   // Initialize serial port
   Serial.begin(115200);
+
+  // Set the initial operating mode
+  operatingMode = NORMAL;
 
   // Configure ports
   setupInputPort();
@@ -77,9 +101,32 @@ void loop() {
 void processSerialInput() {
   if (Serial.available()) {
     serialInChar = Serial.read();
-    writeOutputPort(serialInChar);
-    timeOnsetOut = timeNow;
-    isParallelOutActive = true;
+
+    // deal with the initialization of the pulse length and/or levelmode programming
+    if (operatingMode == NORMAL && serialInChar == 0) {
+      operatingMode = PROGRAMMING;
+    }
+    else if (operatingMode == PROGRAMMING && serialInChar == 1) {
+      operatingMode = PULSELENGTH;
+    }
+    else if (operatingMode == PROGRAMMING && serialInChar == 2) {
+      operatingMode = LEVELMODE;
+    }
+    else if (operatingMode == PULSELENGTH) {
+      // the third byte is the pulse duration
+      TIMEOUT_PULSE_LENGTH = serialInChar;
+      operatingMode = NORMAL;
+    }
+    else if (operatingMode == LEVELMODE) {
+      // the third byte would be the bitmask, but level mode is not supported here
+      operatingMode = NORMAL;
+    }
+    else {
+      // output the incoming character as TTL signals
+      writeOutputPort(serialInChar);
+      timeOnsetOut = timeNow;
+      isParallelOutActive = true;
+    }
   } 
   else if (isParallelOutActive && ((timeNow - timeOnsetOut) > TIMEOUT_PULSE_LENGTH)) {
     writeOutputPort(0);
@@ -112,8 +159,9 @@ void handleDebounceTimeouts() {
 }
 
 void setupOutputPort() {
-  for (int pin = 2; pin <= 9; pin++) {
-    pinMode(pin, OUTPUT);
+  for (int pin = 0; pin <8; pin++) {
+    pinMode(OUTPUT_PINS[pin], OUTPUT);
+    digitalWrite(OUTPUT_PINS[pin], LOW);  // Initialize outputs to LOW
   }
 }
 
