50b80b8019ad759511e3dc6ca01c1ed78b6f0f14
Robert Oostenveld <r.oostenveld@gmail.com>
2026-05-01 11:30:14 +0200

    flexible configuration of boards and channels, updates for Leonardo
    - detect up to 3 boards and assign up to 24 channels to them
    - channel numbering always starts at 1
    - specify frequencies for all channels by a freqoffset and freqstepsize
    - updated I2C speed for Leonardo (max is 400K, not 3.4M)
    - update sampling frequency to 100 Hz, which is the max for the Leonardo and three DACs

--- uno_dac7578/uno_dac7578.ino
+++ uno_dac7578/uno_dac7578.ino
@@ -2,7 +2,7 @@
  * Sketch for an Arduino Uno or Leonardo connected to two Adafruit DAC7578 modules. The purpose is to 
  * act as a MEG phantom driver, controlling a small current through up to 16 magnetic dipole or 
  * equivalent current dipole sources, thereby generating small magnetic fields that can be recorded 
- * using an OPM- or SQUID-based MEG system.
+ * using an OPM or SQUID-based MEG system.
  *
  * See 
  *   https://store.arduino.cc/products/arduino-uno-rev3-smd
@@ -20,26 +20,33 @@ Adafruit_DACX578 dac0(12);
 Adafruit_DACX578 dac1(12);
 Adafruit_DACX578 dac2(12);
 
-const uint8_t nchannel = 24;
+bool dac0Found = false;
+bool dac1Found = false;
+bool dac2Found = false;
 
 const uint8_t addr0 = 0x4C;  // open pads
 const uint8_t addr1 = 0x48;  // left pads
 const uint8_t addr2 = 0x4A;  // right pads
 
-float frequency[24] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
+// these will be updated according to the number of DACs found
+int nchannels = 24;
+int dacBoard[24] = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2 };
+int dacChannel[24] = { 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7 };
+float frequency[24] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 float phase[24] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-const float freqshift = 0;
+float freqoffset = 5;
+float freqstepsize = 0.2;
 
-const uint16_t amplitude = 2048;   // Half of full scale for 12-bit dac0 (0 to 4095)
-const uint16_t offset = 2048;      // DC offset to keep sine wave positive
-const uint32_t sampleTime = 1000;  // Approximate time between samples, in microseconds
-const uint32_t feedbackTime = -1;  // Time between serial feedback, in microseconds
+const float offset = 2048;          // Offset for the 12-bit DAC output (0 to 4095)
+const float amplitude = 2047;       // Half of the full scale for 12-bit DAC (0 to 4095)
+const uint32_t sampleTime = 10000;  // Approximate time between samples, in microseconds
+const uint32_t feedbackTime = -1;   // Time between serial feedback, in microseconds (-1 for no feedback)
 
-void retry(uint32_t duration) {
-  // wait for some time, keep the LED blinking
+void blinkdelay(uint32_t duration) {
+  // wait for some time, but keep the LED blinking
   uint32_t start = millis();
   while ((millis() - start) < duration) {
-    blinker.update();
+    blink.update();
     delay(10);
   }
 };
@@ -50,75 +57,115 @@ void setup() {
   ledFast();
 
   Serial.begin(115200);
-  while (!Serial && (millis() < 5000)) {
-    // wait for the serial interface to start, but not longer than 5 seconds
-    retry(100);
+  while (!Serial && (millis() < 3000)) {
+    // wait for the serial interface to start, but not longer than 3 seconds
+    blinkdelay(200);
   }
 
   Serial.println("-------------------------------");
   Serial.println("Arduino dac7578 sine wave generator");
-  Serial.print("firmware ");
+  Serial.print("Firmware version ");
   Serial.print(__TIME__);
   Serial.print(" / ");
   Serial.println(__DATE__);
 
-  if (nchannel > 0) {
-    // initialize the first DACX578
-    while (!dac0.begin(addr0, &Wire)) {
-      Serial.println("Failed to find dac0");
-      retry(1000);
-    }
-    Serial.println("dac0 initialized");
+  // initialize the first DAC7578
+  dac0Found = dac0.begin(addr0, &Wire);
+  long initstart = millis();
+  while (!dac0Found && (millis() - initstart < 2000)) {
+    blinkdelay(200);
+    dac0Found = dac0.begin(addr0, &Wire);
+  }
+  if (!dac0Found) {
+    Serial.print("Failed to find dac0 at address ");
+    Serial.println(addr0, HEX);
+  } else {
+    Serial.print("Initialized dac0 at address ");
+    Serial.println(addr0, HEX);
   }
 
-  if (nchannel > 8) {
-    // initialize the second DACX578
-    while (!dac1.begin(addr1, &Wire)) {
-      Serial.println("Failed to find dac1");
-      retry(1000);
-    }
-    Serial.println("dac1 initialized");
+  // initialize the second DACC7578
+  dac1Found = dac1.begin(addr1, &Wire);
+  initstart = millis();
+  while (!dac1Found && (millis() - initstart < 2000)) {
+    blinkdelay(200);
+    dac1Found = dac1.begin(addr1, &Wire);
+  }
+  if (!dac1Found) {
+    Serial.print("Failed to find dac1 at address ");
+    Serial.println(addr1, HEX);
+  } else {
+    Serial.print("Initialized dac1 at address ");
+    Serial.println(addr1, HEX);
   }
 
-  if (nchannel > 16) {
-    // initialize the third DACX578
-    while (!dac2.begin(addr2, &Wire)) {
-      Serial.println("Failed to find dac2");
-      retry(1000);
-    }
-    Serial.println("dac2 initialized");
+  // initialize the third DACC7578
+  dac2Found = dac2.begin(addr2, &Wire);
+  initstart = millis();
+  while (!dac2Found && (millis() - initstart < 2000)) {
+    blinkdelay(200);
+    dac2Found = dac2.begin(addr2, &Wire);
+  }
+  if (!dac2Found) {
+    Serial.print("Failed to find dac2 at address ");
+    Serial.println(addr2, HEX);
+  } else {
+    Serial.print("Initialized dac2 at address ");
+    Serial.println(addr2, HEX);
   }
 
-  for (uint8_t channel = 0; channel < nchannel; channel++) {
-    // shift the frequency
-    frequency[channel] += freqshift;
+  // update the channel-to-board mapping
+  for (uint8_t channel = 0; channel < 24; channel++) {
+    // shift each channel to the next board if the current board is not found
+    if (!dac0Found && dacBoard[channel] == 0) {
+      dacBoard[channel]++;
+    }
+    if (!dac1Found && dacBoard[channel] == 1) {
+      dacBoard[channel]++;
+    }
+    if (!dac2Found && dacBoard[channel] == 2) {
+      dacBoard[channel]++;
+    }
+  }
+
+  // update the number of channels according to the number of DAC boards that were found
+  nchannels = 8 * dac0Found + 8 * dac1Found + 8 * dac2Found;
+
+  // set the frequency for each channel
+  for (uint8_t channel = 0; channel < nchannels; channel++) {
+    frequency[channel] = freqoffset + freqstepsize * channel;
     Serial.print("channel ");
     Serial.print(channel + 1);
-    Serial.print(" = ");
+    Serial.print(", board ");
+    Serial.print(dacBoard[channel] + 1);
+    Serial.print(", frequency ");
     Serial.print(frequency[channel]);
     Serial.println(" Hz");
   }
 
-  // set I2C frequency to 3.4 MHz for faster communication
-  Wire.setClock(3400000);
+  // The Arduino Leonardo (based on the ATmega32u4) defaults to an I2C clock speed of 100 kHz and supports a maximum clock speed of 400 kHz
+  Wire.setClock(400000);
 
   // blink slowly for the rest of the time
-  ledSlow();
-}
+  if (nchannels > 0) {
+    ledSlow();
+  }
+
+}  // setup
 
 void loop() {
   static uint32_t lastSample = micros();
   static uint32_t lastFeedback = micros();
 
+  // this is needed to keep the LED blinking
+  blink.update();
+
   uint32_t currentTime = micros();
   uint32_t deltaTime = currentTime - lastSample;
 
-  // this is needed to keep the LED blinking
-  blinker.update();
-
   if (deltaTime >= sampleTime) {
 
-    for (uint8_t channel = 0; channel < nchannel; channel++) {
+    for (uint8_t channel = 0; channel < nchannels; channel++) {
       // increment the phase according to the channels-specific frequency
       phase[channel] += 2.0 * M_PI * frequency[channel] * ((float)deltaTime / 1000000);
       if (phase[channel] >= 2.0 * M_PI) {
@@ -126,18 +173,16 @@ void loop() {
       }
 
       // compute the new value
-      float sineValue = sin(phase[channel]) * amplitude + offset;
+      float sineValue = offset + amplitude * cos(phase[channel]);
 
-      // set the output to the new value
-      if (channel < 8)
-        // channels 0-7 on the first DAC
-        dac0.writeAndUpdateChannelValue(channel, (uint16_t)sineValue);
-      else if (channel < 16)
-        // channels 8-15 correspond to channels 0-7 on the second DAC
-        dac1.writeAndUpdateChannelValue(channel - 8, (uint16_t)sineValue);
-      else if (channel < 24)
-        // channels 16-23 correspond to channels 0-7 on the third DAC
-        dac2.writeAndUpdateChannelValue(channel - 16, (uint16_t)sineValue);
+      // write the value to the corresponding channel of the corresponding dacBoard
+      if (dacBoard[channel] == 0) {
+        dac0.writeAndUpdateChannelValue(dacChannel[channel], (uint16_t)sineValue);
+      } else if (dacBoard[channel] == 1) {
+        dac1.writeAndUpdateChannelValue(dacChannel[channel], (uint16_t)sineValue);
+      } else if (dacBoard[channel] == 2) {
+        dac2.writeAndUpdateChannelValue(dacChannel[channel], (uint16_t)sineValue);
+      }
     }
     lastSample = currentTime;
 
